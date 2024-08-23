559471b66c407805d7a6c4ec7799c4a895060370
Deep191002 <deep.dharmale123@gmail.com>
2024-08-23 15:48:14 +0530

    Your commit message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './component/cart/cart.component';
@@ -9,24 +9,18 @@ import { ProductsComponent } from './component/products/products.component';
 import { FilterPipe } from './shared/filter.pipe';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms'
 //import { from } from 'rxjs';
-@NgModule({
-  declarations: [
-    AppComponent,
-    CartComponent,
-    HeaderComponent,
-    ProductsComponent,
-    FilterPipe
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CartComponent,
+        HeaderComponent,
+        ProductsComponent,
+        FilterPipe
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule], providers: [
+        provideClientHydration(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
